ce2f91562b312f729f60819bcb15d254bc52415f
consultux <45909164+consultux@users.noreply.github.com>
2018-12-16 00:27:50 -0500

    Update gutenacht.py

--- python_scripts/gutenacht.py
+++ python_scripts/gutenacht.py
@@ -1,3 +1,3 @@
-hass.states.set('sensor.tts_input', 'gute nacht grosse', {'language':'de', 'entity': 'media_player.bathroom'})
+hass.states.set('sensor.tts_input', 'gute nacht grosse', {'language':'de', 'entity': 'media_player.lara'})
 time.sleep(25)
 hass.states.set('sensor.tts_input', '', {'language':'de', 'entity': 'media_player.bathroom'})
