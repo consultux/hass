5d9008a11dbbb608569379f1883e00e6865cab2b
Lars <lars@consultux.de>
2018-12-16 00:31:48 -0500

    shorter

--- python_scripts/gutenacht.py
+++ python_scripts/gutenacht.py
@@ -1,3 +1,3 @@
 hass.states.set('sensor.tts_input', 'gute nacht grosse', {'language':'de', 'entity': 'media_player.lara'})
-time.sleep(25)
+time.sleep(15)
 hass.states.set('sensor.tts_input', '', {'language':'de', 'entity': 'media_player.lara'})
