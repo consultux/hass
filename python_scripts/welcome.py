1d85e5d8592af490acafdebc23624e43ee7ca668
Lars <lars@consultux.de>
2019-10-20 09:15:02 -0400

    API changes

--- python_scripts/welcome.py
+++ python_scripts/welcome.py
@@ -1,3 +1,3 @@
-hass.states.set('sensor.tts_input', 'willkommen zu hause', {'language':'de', 'entity': 'media_player.living_room'})
+hass.states.set('sensor.tts_input', 'willkommen zu hause', {'language':'de', 'entity': 'media_player.living_room_2'})
 time.sleep(15)
-hass.states.set('sensor.tts_input', '', {'language':'de', 'entity': 'media_player.living_room'})
+hass.states.set('sensor.tts_input', '', {'language':'de', 'entity': 'media_player.living_room_2'})
